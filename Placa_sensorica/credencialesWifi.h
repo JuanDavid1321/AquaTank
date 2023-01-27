8b9c32316afa392d8ca1abf0a791a9cde72ec638
Juan David Carvajal <jdcarvajal@unicauca.edu.co>
2023-01-26 19:06:05 -0500

    v5.1

--- Placa_sensorica/credencialesWifi.h
+++ Placa_sensorica/credencialesWifi.h
@@ -1,14 +1,6 @@
-const char* ssid     = "Juan David Carvajal";      // SSID
-const char* password = "Juancho1321";      // Password
+const char* ssid     = "xxx";      // SSID
+const char* password = "xxx";      // Password
 
-// const char* ssid     = "MarcElvia";      // SSID
-// const char* password = "R2S3T9S,";      // Password
-
-// const char* ssid     = "TP-LINK_DE16";      // SSID
-// const char* password = "67743667";      // Password
-
-// const char* ssid     = "JACV";      // SSID
-// const char* password = "jacv-777";      // Password
 
 // const char* host = "192.168.0.12";  // Dirección IP local o remota, del Servidor Web .000webhostapp.com
 const char* host = "tanquesinteligentes.000webhostapp.com";  // Dirección IP local o remota, del Servidor Web .000webhostapp.com
